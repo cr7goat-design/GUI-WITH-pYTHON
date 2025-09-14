0d8ec6797f6931dfdc3469e3d161168d8cfb60dd
cr7goat-design <ppir99@aol.com>
2025-09-14 17:46:59 +0100

    Create button.py

---
+++ button.py
@@ -0,0 +1,7 @@
+from tkinter import * 
+win = Tk() 
+win.geometry("200x500")      
+btn = Button(win,text="Click me !", bd=5, background="blue",
+        activebackground="green", activeforeground="white", 
+        command=win.destroy)    
+btn.pack(side="top")  
